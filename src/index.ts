50b8928b1441b8abeb2e435fdeae9ab323309505
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-03 13:50:02 -0700

    WIP. mani-utils. 1. before move to pm-manifest

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './all-types';
 export * from './io';
 export * from './transforms';
 export * from './policy';
+export * from './utils';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,3 @@
+export * from "./1-mani-utils";
+export * from "./json2xml";
 export * from "./uuid";
