b084e98ca3a0408024da02e29245326e03447ffd
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-21 18:24:07 -0700

    WIP. script chunks split. 3. published 1.2.21

--- src/transforms/1-transform-path/4-script/index.ts
+++ src/transforms/1-transform-path/4-script/index.ts
@@ -1,5 +1,5 @@
 export * from "./0-script-parts";
-export * from "./1-chunk-from-string";
-export * from "./2-chunk-to-string";
+export * from "./1-chunk-from-mani";
+export * from "./2-chunk-to-mani";
 export * from "./8-key-names";
 export * from "./9-types";
