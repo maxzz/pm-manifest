a70b34dee8e8948cd6c2babcb3ac5eade8a756ac
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-19 14:31:36 -0800

    WIP. published 1.2.53. fix beautifyXMLCatalog() for single item

--- src/io/1-mani-input/2-beautify-xml-catalog.ts
+++ src/io/1-mani-input/2-beautify-xml-catalog.ts
@@ -3,6 +3,10 @@ import { type CatalogFile } from "../../all-types";
 export function beautifyXMLCatalog(catalog: CatalogFile.Root): CatalogFile.Root {
     catalog.names = (catalog as any)?.names?.name || [];
 
+    if (!Array.isArray(catalog.names)) {
+        catalog.names = [catalog.names];
+    }
+
     catalog.names = catalog.names
         .map((item: any) => item?._attributes)
         .filter(Boolean);
