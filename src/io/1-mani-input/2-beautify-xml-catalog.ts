4c2ba9787118744ea26e9b54d3dc8fe7dc43c023
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-19 14:59:08 -0800

    WIP. published 1.2.55. fix TransformEncoding.xmlRestore for field catalog items

--- src/io/1-mani-input/2-beautify-xml-catalog.ts
+++ src/io/1-mani-input/2-beautify-xml-catalog.ts
@@ -1,4 +1,5 @@
 import { type CatalogFile } from "../../all-types";
+import { TransformEncoding } from "../../transforms";
 
 export function beautifyXMLCatalog(catalog: CatalogFile.Root): CatalogFile.Root {
     catalog.names = (catalog as any)?.names?.name || [];
@@ -10,6 +11,14 @@ export function beautifyXMLCatalog(catalog: CatalogFile.Root): CatalogFile.Root 
     catalog.names = catalog.names
         .map((item: any) => item?._attributes)
         .filter(Boolean);
+
+    //TODO: Check: Was this missing for manifest? and for save as well.
+
+    catalog.names.forEach((item: CatalogFile.ItemInFile) => {
+        item.dispname = item.dispname ? TransformEncoding.xmlRestore(item.dispname) : '';
+        item.value = item.value ? TransformEncoding.xmlRestore(item.value) : '';
+        item.choosevalue = item.choosevalue ? TransformEncoding.xmlRestore(item.choosevalue) : '';
+    });
 
     return catalog;
 }
