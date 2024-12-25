fcf5911d10b574e5976b335326310dcf86af1998
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-24 16:09:42 -0800

    WIP. published 1.2.70. mani-utils code review

--- src/utils/1-mani-utils.ts
+++ src/utils/1-mani-utils.ts
@@ -1,33 +1,29 @@
-import { Mani, Meta } from "../all-types";
+import { type Mani, type Meta } from "../all-types";
 
 // Utilities at form level
 
-export const isManualForm = (form: Meta.Form | undefined): boolean => !!form?.disp.isScript;
-
-export const isWebForm = (form: Meta.Form | undefined): boolean => !!form?.disp.domain;
-
-export const isWhyForm = (form: Meta.Form | undefined): boolean => !!form?.disp.bailOut;
+export const isFormManual = (form: Meta.Form | undefined): boolean => !!form?.disp.isScript;
+export const isFormEmpty = (form: Meta.Form | undefined): boolean => !!form?.disp.noFields;
+export const isFormWeb = (form: Meta.Form | undefined): boolean => !!form?.disp.domain;
+export const isFormIe6 = (form: Meta.Form | undefined): boolean => !!form?.disp.isIe;
+export const isFormWhy = (form: Meta.Form | undefined): boolean => !!form?.disp.bailOut;
 
 // Utilities at meta and manifest level
 
-export const isManual = (meta: Meta.Form[] | undefined): boolean => !!meta?.some(isManualForm);
-
-export const isEmpty = (meta: Meta.Form[] | undefined): boolean => !meta?.length || !!meta?.some((form: Meta.Form) => form.disp.noFields);
-
+export const isAnyManual = (meta: Meta.Form[] | undefined): boolean => !!meta?.some(isFormManual);
+export const isAnyEmpty = (meta: Meta.Form[] | undefined): boolean => !meta?.length || !!meta?.some((form: Meta.Form) => form.disp.noFields);
 export const isAnyWeb = (meta: Meta.Form[] | undefined): boolean => !!meta?.[0]?.disp.domain || !!meta?.[1]?.disp.domain;
-
 export const isAnyIe6 = (meta: Meta.Form[] | undefined): boolean => !!meta?.[0]?.disp.isIe || !!meta?.[1]?.disp.isIe;
-
 export const isAnyWhy = (meta: Meta.Form[] | undefined): boolean => !!meta?.[0]?.disp.bailOut || !!meta?.[1]?.disp.bailOut;
 
-export function isAnyCap(mani: Mani.Manifest | undefined, regex: RegExp | undefined): boolean { // isAnyCaption
+export function isAnyMatchedCap(mani: Mani.Manifest | undefined, regex: RegExp | undefined): boolean { // is any matched caption
     const forms = mani?.forms;
     const form0 = forms?.[0]?.detection?.caption;
     const form1 = forms?.[1]?.detection?.caption;
     return regex ? !!form0?.match(regex) || !!form1?.match(regex) : !!form0 || !!form1;
 }
 
-export function isAnyCls(mani: Mani.Manifest | undefined, regex: RegExp | undefined): boolean { // isAnyClass
+export function isAnyMatchedCls(mani: Mani.Manifest | undefined, regex: RegExp | undefined): boolean { // is any matched class
     const forms = mani?.forms;
     const form0 = forms?.[0]?.detection?.dlg_class;
     const form1 = forms?.[1]?.detection?.dlg_class;
