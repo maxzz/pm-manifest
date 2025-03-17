073b34b24ebf7f608bb6353c44e43f5b662f1654
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-17 14:33:24 -0700

    WIP. published 1.2.76. add buildManiMetaForms2 w/ maniForms param

--- src/io/4-meta-from-file/2-build-meta-forms/0-all.ts
+++ src/io/4-meta-from-file/2-build-meta-forms/0-all.ts
@@ -1,13 +1,35 @@
 import { Mani, Meta } from "../../../all-types";
 import { createMetaForm } from "./1-create-meta-form";
 
-export function buildManiMetaForms(mani: Mani.Manifest | undefined): Meta.Form[] {
+export function buildManiMetaForms(mani: Mani.Manifest | undefined): Meta.Form[] { //TODO: we should not input as mani, but just forms
 
     const metaForms: Meta.Form[] =
         !mani?.forms?.length
             ? []
             : mani.forms.map(createMetaForm);
 
+    updateROtherArray(metaForms);
+
+    return metaForms;
+}
+
+/**
+ * Params are mani, but just forms. buildManiMetaForms() should be removed 
+ * after dropzone, pmac, electron-window-monitor, and pmat24-lite are updated.
+ */
+export function buildManiMetaForms2(maniForms: Mani.Form[] | undefined): Meta.Form[] {
+
+    const metaForms: Meta.Form[] =
+        !maniForms?.length
+            ? []
+            : maniForms.map(createMetaForm);
+
+    updateROtherArray(metaForms);
+
+    return metaForms;
+}
+
+function updateROtherArray(metaForms: Meta.Form[]): void {
     [0, 1].forEach(
         (formIdx: number) => {
             if (metaForms[formIdx]) {
@@ -16,6 +38,4 @@ export function buildManiMetaForms(mani: Mani.Manifest | undefined): Meta.Form[]
             }
         }
     );
-
-    return metaForms;
 }
