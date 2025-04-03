b20cffbb98dfa07a74d7b051d347b5321e8c8e28
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-03 14:46:48 -0700

    WIP. published 1.2.79. new-mani. add createNewManualFormFrom()

--- src/io/5-create-new/0-all-create.ts
+++ src/io/5-create-new/0-all-create.ts
@@ -15,6 +15,25 @@ export function createNewManualForm({ detection, options, fields }: CreateNewMan
     return rv;
 }
 
+/**
+ * Clean up names_ext. We don't use any sub-class names (at least for now) for manual mode forms.
+ * The rule is: don't look inside the window content.
+ * @param form - Form to duplicate from
+ * @returns New duplicated form
+ */
+export function createNewManualFormFrom(form: Mani.Form): Mani.Form {
+
+    const rv: Mani.Form = {
+        detection: { ...form.detection },
+        options: form.options,
+        fields: [],
+    };
+    
+    rv.detection.names_ext = "";
+
+    return rv;
+}
+
 type CreateManualFormDetectionParams = {
     caption: string;
     dlg_class: string;
--- src/io/5-create-new/index.ts
+++ src/io/5-create-new/index.ts
@@ -1,2 +1,1 @@
 export * from './0-all-create';
-export * from './1-create-manual-mode';
