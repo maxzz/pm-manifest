ca5cf539ce9d4a793112cc4d4d885c1b1cd58429
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-06 17:52:58 -0700

    WIP. update createScriptItemByType(). 2

--- src/io/5-create-new/2-script/0-all.ts
+++ src/io/5-create-new/2-script/0-all.ts
@@ -50,6 +50,36 @@ export function createScriptItemByType({ type, password, name }: { type: ChunkKe
     }
 }
 
+// Key, Position, Delay
+
+export function createScriptItem_kbd({ char = 'tab', repeat = 1, shift = 0, ctrl = 0, alt = 0 }: Omit<EditorDataForKbd, 'type'>): EditorDataForKbd {
+    const newItem: EditorDataForKbd = { type: 'kbd', char, repeat, shift, ctrl, alt, };
+    return newItem;
+}
+
+export function createScriptItem_pos({ x = 10, y = 20, units = false, res = 0 }: Omit<EditorDataForPos, 'type'>): EditorDataForPos {
+    const newItem: EditorDataForPos = { type: 'pos', x, y, units, res, };
+    return newItem;
+}
+
+export function createScriptItem_dly({ n = 1000 }: Omit<EditorDataForDly, 'type'>): EditorDataForDly {
+    const newItem: EditorDataForDly = { type: 'dly', n, };
+    return newItem;
+}
+
+// Field
+
+export function createScriptItem_fld({ password, name }: { type: ChunkKey; password: boolean; name: string; }): EditorDataForFld {
+    const field = createForManualMetaField(password, name);
+    const editField = convFieldForEditor(field.mani);
+    const newItem: EditorDataForFld = {
+        type: 'fld',
+        field,
+        editField,
+    };
+    return newItem;
+}
+
 export function createForManualMetaField(password: boolean, name: string): Meta.Field {
     const rv: Meta.Field = {
         mani: createForManualManiField(password, name),
