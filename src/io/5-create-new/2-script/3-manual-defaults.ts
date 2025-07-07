621b4255f71bf2017105006e361b1348f6a366ac
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-06 20:05:18 -0700

    WIP. published v1.4.21. add loginEditorData() and cpassEditorData()

--- src/io/5-create-new/2-script/3-manual-defaults.ts
+++ src/io/5-create-new/2-script/3-manual-defaults.ts
@@ -1,5 +1,7 @@
 import { type Mani } from "../../../all-types";
+import { EditorDataForOne } from "../../../transforms";
 import { createGuid } from "../../../utils";
+import { createScriptItem_pos, createScriptItem_kbd, createScriptItem_fld, createScriptItem_dly } from "./0-all";
 
 export function defaultManualFormFields(createCpass: boolean): Mani.Field[] {
     const rv: Mani.Field[] = createCpass ? cpassFields() : loginFields();
@@ -52,5 +54,35 @@ function cpassFields(): Mani.Field[] {
             path_ext: "[sn]3.2.delay,ms=100;keys,key=tab;field;keys,key=enter;",
             useit: true,
         },
+    ];
+}
+
+export function loginEditorData(): EditorDataForOne[] {
+    return [
+        createScriptItem_pos({ x: 10, y: 10 }),
+        createScriptItem_kbd({ char: 'tab' }),
+        createScriptItem_fld({ password: false, name: 'Username' }),
+        createScriptItem_dly({ n: 100 }),
+        createScriptItem_kbd({ char: 'tab' }),
+        createScriptItem_fld({ password: true, name: 'Password' }),
+    ];
+}
+
+export function cpassEditorData(): EditorDataForOne[] {
+    return [
+        createScriptItem_pos({ x: 10, y: 10 }),
+        createScriptItem_kbd({ char: 'tab' }),
+        createScriptItem_fld({ password: true, name: 'Current password' }),
+        
+        createScriptItem_dly({ n: 100 }),
+        createScriptItem_kbd({ char: 'tab' }),
+        createScriptItem_fld({ password: true, name: 'New password' }),
+
+        createScriptItem_dly({ n: 100 }),
+        createScriptItem_kbd({ char: 'tab' }),
+        createScriptItem_fld({ password: true, name: 'Confirm new password' }),
+
+        createScriptItem_dly({ n: 100 }),
+        createScriptItem_kbd({ char: 'enter' }),
     ];
 }
